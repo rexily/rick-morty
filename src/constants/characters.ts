b8319b5805ae6ecb5f38df31b7eb8e60215768a1
Rexi <rexelions@gmail.com>
2025-11-27 00:57:58 +0300

    карточка персонажа + modern-css-reset

---
+++ src/constants/characters.ts
@@ -0,0 +1,8 @@
+export const RickSanchez = {
+  name: 'Rick Sanchez',
+  gender: 'Male',
+  species: 'Human',
+  location: 'Earth',
+  status: 'Alive',
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+}
